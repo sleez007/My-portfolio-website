cb717c4a12254e87a48cd91f316516d7cb4d0d95
Kingsley Etoka <etokakingsley@gmail.com>
2023-01-14 04:39:32 +0100

    sprinkled a tiny bit of seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,12 @@ import Work from '../src/components/partial/home/Work'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home Page | Kingsley Etoka</title>
+        <meta name="description" content="I'm a Fullstack Web and Mobile App Developer With Over 6 years of experience building Industry Leading Web and Mobile App Solutions with next gen technologies." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main>
        <Hero />
        <Work />
