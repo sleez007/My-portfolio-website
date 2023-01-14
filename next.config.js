2a13683d5e19a482e6a2aef517baf73b763cde3f
Kingsley Etoka <etokakingsley@gmail.com>
2023-01-14 20:34:30 +0100

    Added pwa support

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register:true,
+  skipWaiting:true,
+  disable:process.env.NODE_ENV === 'development'
+})
+
+module.exports = withPWA({
+  // next.js config
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-}
+})
+// const nextConfig = {
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true,
+//   },
+// }
 
-module.exports = nextConfig
+//module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,13 +22,17 @@ export default function App({ Component, pageProps }: AppProps) {
             },
             images: [
               {
-                url: '../public/assets/images/kingsley.png',
+                url: '/assets/images/kingsley.png',
                 width: 850,
                 height: 650,
                 alt: 'Profile Photo',
               },
             ],
           }}
+        />
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
         />
       <Component {...pageProps} />
     </ThemeHookProvider>
