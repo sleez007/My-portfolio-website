e42fd68a69e0319bba45ea91485386609075f20b
Kingsley Etoka <33032817+sleez007@users.noreply.github.com>
2023-09-07 04:32:58 +0100

    Update Portfolio.tsx

--- src/components/partial/home/Portfolio.tsx
+++ src/components/partial/home/Portfolio.tsx
@@ -28,7 +28,7 @@ const projects: Wp = {
         {
             name: 'Kanban Board',
             description: 'This is a drag and drop project management board to keep track of tasks and their status',
-            stack: ['Micro Frontend', 'Nx mono Repo', 'Angular', 'Domain Driven Design', 'NGRX', 'RXJS', 'SASS',],
+            stack: ['Micro Frontend', 'Nx mono Repo', 'Angular', 'DDD', 'Jest & Rxjs Marble', 'NGRX', 'RXJS', 'SASS',],
             cover: kanban,
             environment: [
                 {
